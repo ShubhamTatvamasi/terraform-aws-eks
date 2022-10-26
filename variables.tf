2289a5b07892edc82703ff7c958606779f406772
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-10-26 21:46:43 +0530

    modified:   variables.tf

--- variables.tf
+++ variables.tf
@@ -13,14 +13,14 @@ variable "instance_types" {
   default = ["t3a.large"]
 }
 
-variable "worker_nodes_min_size" {
-  type    = number
-  default = 1
-}
-
 variable "disk_size" {
   type    = number
   default = 50
+}
+
+variable "worker_nodes_min_size" {
+  type    = number
+  default = 1
 }
 
 variable "worker_nodes_max_size" {
