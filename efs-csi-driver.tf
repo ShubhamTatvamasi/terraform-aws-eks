a3aac39dfb925cc345866b03cf1c95923e7362f8
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-10-27 00:10:41 +0530

    modified:   efs-csi-driver.tf

--- efs-csi-driver.tf
+++ efs-csi-driver.tf
@@ -6,24 +6,33 @@ resource "helm_release" "efs_csi_driver" {
   namespace  = "kube-system"
 
   values = [<<VALUES
+  image:
+    repository: 602401143452.dkr.ecr.ap-south-1.amazonaws.com/eks/aws-efs-csi-driver
   controller:
-    serviceAccount:
-      create: true
-      name: "aws-efs-csi-driver"
-      annotations:
-        eks.amazonaws.com/role-arn: ${aws_iam_role.efs_csi_driver.arn}
-  node:
     serviceAccount:
       create: false
       name: "aws-efs-csi-driver"
       annotations:
         eks.amazonaws.com/role-arn: ${aws_iam_role.efs_csi_driver.arn}
+  node:
+    serviceAccount:
+      create: true
+      name: "aws-efs-csi-driver"
+      annotations:
+        eks.amazonaws.com/role-arn: ${aws_iam_role.efs_csi_driver.arn}
   storageClasses:
     - name: efs-sc
+      mountOptions:
+      - tls
       parameters:
         provisioningMode: efs-ap
         fileSystemId: ${aws_efs_file_system.eks_pv.id}
         directoryPerms: "700"
+        gidRangeStart: "1000"
+        gidRangeEnd: "2000"
+        basePath: "/dynamic_provisioning"
+      reclaimPolicy: Delete
+      volumeBindingMode: Immediate
   VALUES
   ]
 }
