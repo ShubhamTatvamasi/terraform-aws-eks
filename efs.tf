583ac27337c73b9af5df8a6c1d518a7eae0d39b9
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-10-27 11:00:14 +0530

    modified:   efs.tf

--- efs.tf
+++ efs.tf
@@ -7,17 +7,17 @@ resource "aws_efs_file_system" "eks" {
 resource "aws_efs_mount_target" "eks_mnt_0" {
   file_system_id = aws_efs_file_system.eks.id
   subnet_id      = data.aws_subnets.default.ids.0
-  security_groups = [module.eks_cluster.cluster_security_group_id]
+  security_groups = [aws_security_group.efs.id]
 }
 
 resource "aws_efs_mount_target" "eks_mnt_1" {
   file_system_id = aws_efs_file_system.eks.id
   subnet_id      = data.aws_subnets.default.ids.1
-  security_groups = [module.eks_cluster.cluster_security_group_id]
+  security_groups = [aws_security_group.efs.id]
 }
 
 resource "aws_efs_mount_target" "eks_mnt_2" {
   file_system_id = aws_efs_file_system.eks.id
   subnet_id      = data.aws_subnets.default.ids.2
-  security_groups = [module.eks_cluster.cluster_security_group_id]
+  security_groups = [aws_security_group.efs.id]
 }
