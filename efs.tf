3e842cd3846e1b087bb75ae079a3cec685b243fc
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-10-27 11:04:30 +0530

    modified:   efs.tf

--- efs.tf
+++ efs.tf
@@ -5,19 +5,19 @@ resource "aws_efs_file_system" "eks" {
 }
 
 resource "aws_efs_mount_target" "eks_mnt_0" {
-  file_system_id = aws_efs_file_system.eks.id
-  subnet_id      = data.aws_subnets.default.ids.0
+  file_system_id  = aws_efs_file_system.eks.id
+  subnet_id       = data.aws_subnets.default.ids.0
   security_groups = [aws_security_group.efs.id]
 }
 
 resource "aws_efs_mount_target" "eks_mnt_1" {
-  file_system_id = aws_efs_file_system.eks.id
-  subnet_id      = data.aws_subnets.default.ids.1
+  file_system_id  = aws_efs_file_system.eks.id
+  subnet_id       = data.aws_subnets.default.ids.1
   security_groups = [aws_security_group.efs.id]
 }
 
 resource "aws_efs_mount_target" "eks_mnt_2" {
-  file_system_id = aws_efs_file_system.eks.id
-  subnet_id      = data.aws_subnets.default.ids.2
+  file_system_id  = aws_efs_file_system.eks.id
+  subnet_id       = data.aws_subnets.default.ids.2
   security_groups = [aws_security_group.efs.id]
 }
